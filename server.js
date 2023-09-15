59e3a174e5f2df2eea94e080adfd199111f682d6
Hromas <sharovmail@mail.ru>
2023-09-16 00:13:38 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const server = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+server.all('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>');
+  res.end();
+})
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
+function keepAlive() {
+  server.listen(3000, () => { console.log("Server is online!") });
+}
 
 module.exports = keepAlive;
